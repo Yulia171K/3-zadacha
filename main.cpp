f4594ed88d9bd2fe656c3654ca6bc01e576e1ffe
yul <guj>
2022-11-07 19:55:25 +0700

    добавила комментарии

--- main.cpp
+++ main.cpp
@@ -1,78 +1,36 @@
-#include <iostream>
-#include <cmath> 
-#include <string>
-using namespace std;
+#include <iostream> //подключение библиотеки iostream
+#include <cmath> //подключение библиотеки математики
+#include <string>//подключение string
+using namespace std;//глобальное подключение std
 
-
+//функция ввода в программу
 int main()
 { 
-    int U = 6000;
-    int G = 360;
+    int U = 6000;//переменная типа int
+    int G = 360;//переменная типа int
 
-    div_t divresult;
-
-
-    string R;
-    string UG = " Ugol ";
-    string Rav = " Raven ";
-    string Tir = " - ";
-
-    int d;
-    int a;
-    int b;
+    div_t divresult;//переменная типа div_t
 
 
+    string R;//переменная типа string
+    string UG = " Ugol ";//переменная типа string
+    string Rav = " Raven ";//переменная типа string
+    string Tir = " - ";//переменная типа string
+
+    int d;//переменная типа int
+    int a;//переменная типа int
+    int b;//переменная типа int
 
     
-    cout << " enter degree " << endl;
-    cin >> d;
+    cout << " enter degree " << endl;//вывод строки в консоль
+    cin >> d;//ввод в консоль
 
-     divresult = div(U, G);
-     b = (divresult.quot * d);
-     b = fmod(b, 100);
+     divresult = div(U, G);//вычисление
+     b = (divresult.quot * d);//вычисление
+     b = fmod(b, 100);//вычисление
 
-     divresult = div((divresult.quot * d), 100);
-     a = divresult.quot;
+     divresult = div((divresult.quot * d), 100);//вычисление
+     a = divresult.quot;//вычисление
 
-    cout << UG << d << Rav << a << Tir << b;
-
-
-
-
-
-
-
-//     string R;
-//     int I;
-//     int Mb;
-//     int Kb;
-//     int B;
-//     div_t divresult;
-
-//     string mb = " Mbyte ";
-//     string kb = " Kbyte ";
-//     string b = " byte ";
-
-//     cout << "enter number" << endl;
-//     cin >> I;
-
-//     B = fmod(I, 1024);
-//     divresult = div(I, 1024);
-//     Kb = divresult.quot; 
-//     Kb = fmod(Kb, 1024);
-//     divresult = div(Kb, 1024);
-//     Mb = divresult.quot;
-
-//    cout << Mb << mb << Kb << kb << B << b;
-
-
-
-
-
-
-
-
-
-
-
+    cout << UG << d << Rav << a << Tir << b;//вывод результата
 }
